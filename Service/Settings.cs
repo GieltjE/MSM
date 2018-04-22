47f8917a079343fadedde7f6b02003051dd3b047
GieltjE <m.hazelhof@fyn.nl>
2018-04-22 14:53:49 +0200

    Service.Settings Don't forget to set the dirty flag to false

--- Service/Settings.cs
+++ Service/Settings.cs
@@ -80,6 +80,7 @@ namespace MSM.Service
                     XMLSerializer.Serialize(writer, SettingsClass);
                     writer.Flush();
                 }
+                SettingsClass.Dirty = false;
             }
         }
     }
