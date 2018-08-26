d24543721e1dfb0eee4a39619c84319aad8d08fe
GieltjE <m.hazelhof@fyn.nl>
2018-08-26 13:28:25 +0200

    Restore license text

--- About.Designer.cs
+++ About.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace MSM
+namespace MSM
 {
     partial class About
     {
@@ -47,7 +47,7 @@
             this.richTextBox1.ReadOnly = true;
             this.richTextBox1.Size = new System.Drawing.Size(675, 171);
             this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            this.richTextBox1.Text = "    This program is free software: you can redistribute it and/or modify\n    it under the terms of the GNU General Public License as published by\n    the Free Software Foundation, either version 3 of the License, or\n    (at your option) any later version.\n\n    This program is distributed in the hope that it will be useful,\n    but WITHOUT ANY WARRANTY; without even the implied warranty of\n    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n    GNU General Public License for more details.\n\n    You should have received a copy of the GNU General Public License\n    along with this program.  If not, see <http://www.gnu.org/licenses/>.";
             // 
             // groupBox1
             // 
