9e560ce2b1d39fd38188749033fe0538bb1834b8
Michiel Hazelhof <michiel@hazelhof.nl>
2021-04-13 21:23:30 +0200

    Add build script

---
+++ build.ps1
@@ -0,0 +1,21 @@
+$bindir="C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin"
+
+& $bindir\MSBuild.exe /nr:True .\MSM.sln /target:Clean /p:Platform=x64 /p:Configuration=Release
+& $bindir\MSBuild.exe /nr:True .\MSM.sln /target:Restore /p:Platform=x64 /p:Configuration=Release
+& $bindir\MSBuild.exe /nr:True .\MSM.sln /target:Rebuild /p:Platform=x64 /p:Configuration=Release
+
+& $bindir\MSBuild.exe /nr:True .\MSM.sln /target:Clean /p:Platform=x86 /p:Configuration=Release
+& $bindir\MSBuild.exe /nr:True .\MSM.sln /target:Restore /p:Platform=x86 /p:Configuration=Release
+& $bindir\MSBuild.exe /nr:True .\MSM.sln /target:Rebuild /p:Platform=x86 /p:Configuration=Release
+
+$version = (get-item -Path 'bin\x64\Release\MSM.exe').VersionInfo.ProductVersion
+
+Remove-Item Release -Recurse -Force
+New-Item -Path "." -Name "Release" -ItemType "directory"
+
+$sevenzipdir="C:\Program Files\7-Zip"
+& $sevenzipdir\7z.exe a -t7z ".\Release\v$version-x64.7z" ".\bin\x64\Release\*" -mx9 -y
+& $sevenzipdir\7z.exe a -t7z ".\Release\v$version-x86.7z" ".\bin\x64\Release\*" -mx9 -y
+
+& $sevenzipdir\7z.exe a -tzip ".\Release\v$version-x64.zip" ".\bin\x86\Release\*" -mx9 -y
+& $sevenzipdir\7z.exe a -tzip ".\Release\v$version-x86.zip" ".\bin\x86\Release\*" -mx9 -y
